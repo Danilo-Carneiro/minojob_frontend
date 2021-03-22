fb26db72e91bfbc7dbfa2bd6a52ffbdfa7d1a752
Matheus Rangel <Matheus.rangelanatovitz@gmail.com>
2021-03-22 12:01:04 -0300

    Adicionado barra de pesquisa

--- src/pages/VagaPage.tsx
+++ src/pages/VagaPage.tsx
@@ -1,5 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, ScrollView, TextInput, Dimensions} from 'react-native'
+import {
+    View, 
+    StyleSheet, 
+    ScrollView, 
+    TextInput, 
+    Dimensions,
+    Text} from 'react-native'
 import CardVaga, {Vaga} from '../components/itemVaga'
 import Icon from 'react-native-vector-icons/Ionicons';
 import api from '../services/api';
@@ -12,13 +18,37 @@ export default function VagaPage() {
         useEffect(() => {
             api.get('/vaga').then((response )=> {
                 setVaga(response.data)
+                console.log("Lista Carregada")
             })
         }, [])
     
     return(
         <>
                 <Header/>
-               
+
+                <View style={{flexDirection:'row',
+                            justifyContent:'center',
+                            paddingHorizontal: 20,
+                            backgroundColor: '#ebebeb'
+                            }}>
+                    <TextInput 
+                    placeholder='Pesquisar'
+                    style={styles.pesquisa}
+                    />
+                    <TouchableHighlight
+                    underlayColor="#999"
+                    onPress={() => alert('Pesquisar')}
+                    style={styles.btnPesquisa}>
+                        <Text >
+                            <Icon 
+                            name="search" 
+                            size={30} 
+                            color="#000" 
+                            />
+                        </Text>
+                    </TouchableHighlight>
+                        
+                </View>
                 <View style={styles.container}>
                     <ScrollView style={{height: '100%'}}>
 
@@ -38,6 +68,23 @@ export default function VagaPage() {
 
 const styles = StyleSheet.create({
     container: {
-        height: Dimensions.get('window').height-100
+        height: Dimensions.get('window').height-170,
+        marginBottom: 60,
+    },
+    pesquisa: {
+        width: '90%',
+        height: 50,
+        backgroundColor: '#ebebeb',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 8
+    },
+    btnPesquisa: {
+        width: 75,
+        height: 50,
+        backgroundColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
     }
 })
